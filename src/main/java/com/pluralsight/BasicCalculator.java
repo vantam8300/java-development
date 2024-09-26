e0c1ae9b5ec5499298332ce880a5f56d1b656b20
Van Tam Chau <vantam8300@gmail.com>
2024-09-26 13:53:07 -0700

    Completed Mod3 - Exercise 3 - Basic Calculator

---
+++ src/main/java/com/pluralsight/BasicCalculator.java
@@ -0,0 +1,49 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        int firstNum = scanner.nextInt();
+        System.out.print("Enter the Second number: ");
+        int secondNum = scanner.nextInt();
+
+        System.out.println("Possible calculations: ");
+        System.out.println("(A)dd");
+        System.out.println("(S)ubtract");
+        System.out.println("(M)ultify");
+        System.out.println("(D)ivide");
+
+        String operation = scanner.next();
+
+        System.out.println("Please select an option: " + operation);
+        System.out.println(operation);
+        int result = 0;
+        String sign = "";
+        switch (operation) {
+            case "A":
+                result = firstNum + secondNum;
+                sign = "+";
+                break;
+            case "S":
+                result = firstNum - secondNum;
+                sign = "-";
+                break;
+            case "M":
+                result = firstNum * secondNum;
+                sign = "*";
+                break;
+            case "D":
+                result = firstNum / secondNum;
+                sign = "/";
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid Operation");
+        }
+
+        System.out.println(firstNum + " " + sign + " " + secondNum + " = " + result);
+    }
+}
+
